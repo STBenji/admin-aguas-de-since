290b8b50c78285c83c2fc73394329131201a7980
ConanGH-S <blandon0207s@gmail.com>
2024-02-01 19:04:47 -0500

    feat: protected routes for dashboard
    you have to login before access to dashboard

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,23 +1,23 @@
 import { Outlet, useNavigate } from 'react-router-dom'
 import NavbarComponent from './components/Navbar'
-import useAuthentication from './hooks/useAuthentication'
-import { useEffect } from 'react'
+import axios from 'axios'
+import useSWR from 'swr'
+
+const fetcher = (url: string) => axios.post(url, {}, { withCredentials: true }).then((res) => res.data)
 
 export default function Layout() {
-  const { auth } = useAuthentication()
   const navigate = useNavigate()
+  const { error } = useSWR('http://localhost:3000/api/v1/users/auth', fetcher)
 
-  useEffect(() => {
-    console.log(auth)
-    if (!auth) {
-      navigate('/login')
-    }
-  }, [])
+  if (error) {
+    navigate('/login')
+    return null
+  }
 
-  return auth ? (
+  return (
     <div>
       <NavbarComponent />
       <Outlet />
     </div>
-  ) : null
+  )
 }
--- src/hooks/useAuthentication.tsx
+++ src/hooks/useAuthentication.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react'
 import cookie from 'js-cookie'
 import { useLocation } from 'react-router'
+import axios from 'axios'
 
 export default function useAuthentication() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null)
   const location = useLocation()
   const session = cookie.get('user_session')
 
+  axios.post(`${import.meta.env.VITE_API_ROUTE}/v1/users/auth`, {}, { withCredentials: true }).then((res) => {
+    console.log(res)
+  })
   console.log(session)
   console.log(document.cookie)
 
